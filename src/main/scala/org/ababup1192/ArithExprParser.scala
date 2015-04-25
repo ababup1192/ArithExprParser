c1b4b513f602bd3cd394064f9c76fa4ff82aab02
ababup1192 <ababup1192@gmail.com>
2015-04-25 23:17:44 +0900

    Implement arith expr parser that return Abstract syntax tree

---
+++ src/main/scala/org/ababup1192/ArithExprParser.scala
@@ -0,0 +1,35 @@
+package org.ababup1192
+
+import scala.util.parsing.combinator.RegexParsers
+
+trait AST
+
+case class AddOp(left: AST, right: AST) extends AST
+
+case class SubOp(left: AST, right: AST) extends AST
+
+case class MulOp(left: AST, right: AST) extends AST
+
+case class IntVal(value: Int) extends AST
+
+
+object ArithExprParser extends RegexParsers {
+
+  def intLiteral: Parser[AST] = """[1-9][0-9]*|0""".r ^^ {
+    case value => IntVal(value.toInt)
+  }
+
+  def expr: Parser[AST] = chainl1(term, calc("+") | calc("-"))
+
+  def calc(operand: String) = operand ^^ { op => (left: AST, right: AST) =>
+    if (op == "+") AddOp(left, right)
+    else if (op == "-") SubOp(left, right)
+    else IntVal(0)
+  }
+
+  def term: Parser[AST] = chainl1(factor, "*" ^^ { case op => (l: AST, r: AST) => MulOp(l, r) })
+
+  def factor: Parser[AST] = intLiteral | "(" ~> expr <~ ")"
+
+  def parse(input: String): ParseResult[Any] = parseAll(expr, input)
+}
---
+++ src/test/scala/org/ababup1192/ParserSpec.scala
@@ -0,0 +1,11 @@
+package org.ababup1192
+
+import org.scalatest._
+
+class ParserSpec extends FlatSpec with Matchers {
+  "ArithExpr parser" should "return AST" in {
+    val parseResult = ArithExprParser.parse("((4 + 2) * 3) - 6")
+    parseResult.get should be(SubOp(MulOp(AddOp(IntVal(4), IntVal(2)), IntVal(3)), IntVal(6)))
+  }
+}
+
